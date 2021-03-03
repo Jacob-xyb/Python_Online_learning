7e028a3d2c92903e91f5d9772824ba395bdd03a9
Jacob-xyb <949782197@qq.com>
2021-03-03 15:03:09 +0800

    add tasaitacai sklearn

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Python_Online_learning
 Miscellaneous learning and sharing
+
+## bilibili_fishc_python基础
+[(https://www.bilibili.com/video/BV1Fs411A7HZ?p=1)]
---
+++ bilibili_fishc_python基础/004_断言(assert).py
@@ -0,0 +1,12 @@
+"""
+1  断言(assert)
+当assert关键字后边的条件为假的时候，程序自动崩溃并抛出AssertionError的异常。
+一般用来在程序中置入检查点，当需要确保程序中的某个条件一定为真才能让程序正常工作时非常有用。
+"""
+
+assert 3 > 4
+
+# output:AssertionError
+
+
+
